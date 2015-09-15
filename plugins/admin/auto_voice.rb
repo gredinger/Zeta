dd465ca00c767d745f71b5e619b3f2c8420ac892
Liothen <liothen@flagrun.net>
2015-09-15 12:31:29 -0700

    auto voice users that talk, devoice them after an hour of not talking

--- plugins/admin/auto_voice.rb
+++ plugins/admin/auto_voice.rb
@@ -9,25 +9,35 @@ module Admin
         plugin_name: "BotAuto",
         help:        "Listen's for automatic modes",
     )
-    listen_to :join
+    listen_to :channel
     match /autovoice (on|off)$/
 
     # Initialization
     def initialize(*args)
-      @channels = Zchannel.where(auto_voice: true).map(:name)
       super
+      @channels = Zchannel.where(auto_voice: true).map(:name)
+      @users = {}
     end
 
     def listen(m)
       if @channels.member? m.channel
         unless m.user.nick == bot.nick
-          m.channel.voice(m.user) if m.channel.opped? bot.nick
+          # m.channel.voice(m.user) if m.channel.opped? bot.nick
+          if defined? m.user.authname && m.user.nick != bot.nick
+            @users[m.user.authname] = Time.now
+            if m.channel.opped? bot.nick
+              unless m.channel.opped?(m.user) || m.channel.voiced?(m.user) || m.channel.half_opped?(m.user)
+                m.channel.voice(m.user)
+              end
+            end
+          end
         end
+        # Check for timed out, thus devoice user
+        timer(m)
       end
     end
 
     def execute(m, option)
-      # return false unless check_user(m, :operator)
       c = check_channel(m)
       if option == 'on'
         Zchannel.where(id: c.id).update(auto_voice: true)
@@ -39,7 +49,21 @@ module Admin
         m.reply 'Autovoice Disabled'
       end
     end
+
+
+    def timer(m)
+      @users.delete_if do |k,v|
+        v <= Time.now - 3600
+      end
+      puts @users
+      puts m.channel.voiced
+      m.channel.voiced.each do |v|
+        debug 'Check users'
+        m.channel.devoice(v) unless @users.has_key?(v.authname)
+      end
+    end
   end
+
 end
 
 
