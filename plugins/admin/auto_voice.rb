5431b49a65d823f6997249496a3a2a2f046cbf69
Liothen <liothen@flagrun.net>
2015-09-16 11:58:19 -0700

    auto voice now respects ignored users

--- plugins/admin/auto_voice.rb
+++ plugins/admin/auto_voice.rb
@@ -10,24 +10,24 @@ module Admin
         help:        "Listen's for automatic modes",
     )
     listen_to :channel
-    match /autovoice (on|off)$/
+    match /autovoice (on|off|refresh)$/
 
     # Initialization
     def initialize(*args)
       super
       @channels = Zchannel.where(auto_voice: true).map(:name)
+      @ignore = Zuser.where(ignore: true).map(:authname)
       @users = {}
     end
 
     def listen(m)
       if @channels.member? m.channel
+        return if defined?(m.user.authname) && @ignore.member?(m.user.authname)
+        puts @ignore
         unless m.user.nick == bot.nick
-          # m.channel.voice(m.user) if m.channel.opped? bot.nick
           if defined? m.user.authname && m.user.nick != bot.nick
-
             @users[m.channel] = Hash.new unless @users.has_key?(m.channel)
             @users[m.channel][m.user.authname] = Time.now
-
             if m.channel.opped?(bot.nick) || m.channel.half_opped?(bot.nick)
               unless m.channel.opped?(m.user) || m.channel.voiced?(m.user) || m.channel.half_opped?(m.user)
                 m.channel.voice(m.user)
@@ -35,6 +35,7 @@ module Admin
             end
           end
         end
+
         # Check for timed out, thus devoice user
         timer(m)
       end
@@ -45,10 +46,15 @@ module Admin
       if option == 'on'
         Zchannel.where(id: c.id).update(auto_voice: true)
         @channels = Zchannel.where(auto_voice: true).map(:name)
+        @ignore = Zuser.where(ignore: true).map(:authname)
         m.reply 'Autovoice Enabled!'
+      elsif option == 'refresh'
+        @ignore = Zuser.where(ignore: true).map(:authname)
+        m.reply 'Autovoice refreshed'
       else
         Zchannel.where(id: c.id).update(auto_voice: false)
         @channels = Zchannel.where(auto_voice: true).map(:name)
+        @ignore = Zuser.where(ignore: true).map(:authname)
         m.reply 'Autovoice Disabled'
       end
     end
